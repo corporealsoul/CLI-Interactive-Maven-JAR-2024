834f363a5ccfb80c120ce4e74be8bd7dbb70801a
Anup Kumar Mondal <bca.anup@gmail.com>
2024-07-22 11:14:43 +0000

    CLI-Interactive-Maven-WAR-2024

---
+++ src/test/java/com/cliinteractivemavenjar2024/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.cliinteractivemavenjar2024.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
